863f66304f0101bddcb415d96917e73f07dc2b03
supanat_k <supanat_k@tripetch-it.co.th>
2023-05-16 16:14:00 +0700

    use useReducer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,6 +7,7 @@ function Sidebar() {
     { label: "Buttons", path: "/buttons" },
     { label: "Modal", path: "/modal" },
     { label: "Table", path: "/table" },
+    { label: "Count", path: "/count" },
   ];
 
   const renderedLinks = links.map((link) => {
